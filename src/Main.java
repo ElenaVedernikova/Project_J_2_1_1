f64170eff73a91f0695579b3be07ae599654da22
ElenaVedernikova <lenok-v@live.ru>
2021-05-14 18:47:24 +0300

    “commit”

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2000;
+        int milePrice = 20;
+        int mileAmount  = ticketPrice/milePrice;
+        System.out.println("Total mile: " + mileAmount);
+    }
+}
